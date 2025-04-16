2ff00f5dd83541bd739cc6c8fe536f0df59e2460
ThuyTien2307 <tranthithuytien2307@gmail.com>
2025-04-16 20:26:12 +0700

    edit

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import * as userController from "../controllers/users.controller.js";
+import userController from "../controllers/users.controllers.js"
 
 const router = Router();
 
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.get("/", userController.getAll);
+router.get('/id', userController.getDetail);
+router.post("/", userController.postUser);
+router.put('/:id', userController.putUser);
+router.delete('/:id', userController.deleteUser);
 
 export default router;
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,48 +1,41 @@
-import userService from '../apis/users/user.service.js';
 import Database from '../database/users.database.js';
 
-class UserService{
-
-getAllUsers(){ 
+class UserService {
+  getAllUsers() {
     return Database.getUsers();
-}
- 
-getDetail(id){
-    const mockUser = Database.getUsers(); 
-    return mockUser.find((u) => u.id === Number(req.params.id));
-    
-}
+  }
 
-postUser(name){
+  getDetail(id) {
+    const mockUser = Database.getUsers();
+    return mockUser.find((u) => u.id === Number(id));
+  }
+
+  postUser(name) {
     const mockUser = Database.getUsers();
     const id = mockUser.length > 0 ? mockUser[mockUser.length - 1].id + 1 : 1;
     const newUser = { id, name };
-    
     mockUser.push(newUser);
-    userService.setUsers(mockUser);
+    Database.setUsers(mockUser);
     return newUser;
-}
+  }
 
-putUser(userId,name){
+  putUser(userId, name) {
     const mockUser = Database.getUsers();
-    const user = mockUser.find((u) => u.id === userId);
+    const user = mockUser.find((u) => u.id === Number(userId));
+    if (!user) return null;
     user.name = name;
-    userService.setUsers(mockUser);
-    return res.json(user);
-}
+    Database.setUsers(mockUser);
+    return user;
+  }
 
-deleteUser(idUser) {
-    const mockUser = Database.getUsers(); 
-   
-    const userIndex = mockUser.findIndex((u) => u.id === idUser);
-    if (userIndex == -1){
-        return null;
-    }
-    const deleteUser = mockUser.splice(userIndex, 1);
-    userService.setUsers(mockUser);
-    return deleteUser[0];
-}
-
+  deleteUser(idUser) {
+    const mockUser = Database.getUsers();
+    const userIndex = mockUser.findIndex((u) => u.id === Number(idUser));
+    if (userIndex === -1) return null;
+    const deletedUser = mockUser.splice(userIndex, 1)[0];
+    Database.setUsers(mockUser);
+    return deletedUser;
+  }
 }
 
 export default new UserService();
