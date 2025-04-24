8045cc7453d925112cd70d0da3a83f488f00e77c
ThuyTien2307 <tranthithuytien2307@gmail.com>
2025-04-24 19:15:48 +0700

    add check-user

---
+++ src/src/middleware/validate.middleware.js
@@ -0,0 +1,55 @@
+import Joi from "joi";
+
+class ValidateMiddleware {
+    async validateId(req, res, next) {
+        try {
+            const schema = Joi.object({
+                id: Joi.number().required()
+            });
+
+            await schema.validateAsync(req.params, { abortEarly: false });
+            next();
+        } catch (err) {
+            res.status(400).json({
+                success: false,
+                message: "Invalid ID"
+            });
+        }
+    }
+
+    async validatePassword(req, res, next) {
+        try {
+            const schema = Joi.object({
+                password: Joi.string().required()
+            });
+
+            await schema.validateAsync(req.body, { abortEarly: false });
+            next();
+        } catch (err) {
+            res.status(400).json({
+                success: false,
+                message: "Invalid Password"
+            });
+        }
+    }
+
+    async validateUser(req, res, next) {
+        try {
+            const schema = Joi.object({
+                email: Joi.string().email().required(),
+                password: Joi.string().min(6).required()
+            });
+
+            await schema.validateAsync(req.body, { abortEarly: false });
+            next();
+        } catch (err) {
+            res.status(400).json({
+                success: false,
+                message: "Invalid User Data",
+                details: err.details.map(e => e.message)
+            });
+        }
+    }
+}
+
+export default new ValidateMiddleware();
---
+++ src/src/model/users.model.js
@@ -0,0 +1,72 @@
+import { ObjectId, ReturnDocument } from "mongodb";
+import { getDB } from "../config/db.config.js";
+
+class UserModule {
+
+    async createUser(name, password, email) {
+        try {
+            const user = await getDB().collection('user').insertOne({
+                name: name.trim(),
+                password: password.trim(),
+                email: email.trim()
+            });
+            return user.insertedId;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async getAllUsers() {
+        try {
+            return await getDB().collection('user').find().toArray();
+        } catch (err) {
+            throw err;
+        }
+    }
+    async getUser(email, password){
+        try{
+            const user =  await getDB().collection('user').findOne({email: email, password: password})
+            
+            return user;
+        } catch(err){
+            throw err;
+        }
+    }
+    async getUserById(_id){
+        try {
+            return await getDB().collection('user').findOne({  _id: new ObjectId(_id)  });
+        } catch (err) {
+            throw err;
+        }
+    }
+    // async getDetail(_id) {
+    //     try {
+    //         return await getDB().collection('user').findOne({ _id: new ObjectId(_id) });
+    //     } catch (err) {
+    //         throw err;
+    //     }
+    // }
+
+    // async putUser(name, _id) {
+    //     try {
+    //         const user = await getDB().collection('user').findOneAndUpdate(
+    //             { _id: new ObjectId(_id) },
+    //             { $set: { name: name.trim() } },
+    //             { returnDocument: ReturnDocument.AFTER }
+    //         );
+    //         return user.value;
+    //     } catch (err) {
+    //         throw err;
+    //     }
+    // }
+
+    // async deleteUser(_id) {
+    //     try {
+    //         return await getDB().collection('user').findOneAndDelete({ _id: new ObjectId(_id) });
+    //     } catch (err) {
+    //         throw err;
+    //     }
+    // }
+}
+
+export default new UserModule();
